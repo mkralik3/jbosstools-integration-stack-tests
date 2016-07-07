42b502bbe41f92e0a130793eaa09ef429cf25182
mkralik3 <mkralik@redhat.com>
2016-07-07 13:46:48 +0200

    refactor ldap wizard

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/reddeer/manager/ImportManager.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/reddeer/manager/ImportManager.java
@@ -2,11 +2,7 @@ package org.jboss.tools.teiid.reddeer.manager;
 
 import java.util.Properties;
 
-import org.jboss.tools.teiid.reddeer.ModelProject;
-import org.jboss.tools.teiid.reddeer.wizard.imports.LdapImportWizard;
 import org.jboss.tools.teiid.reddeer.wizard.imports.SalesforceImportWizard;
-import org.jboss.tools.teiid.reddeer.wizard.imports.TeiidImportWizard;
-import org.jboss.tools.teiid.ui.bot.test.TeiidBot;
 
 public class ImportManager {
 
@@ -15,12 +11,6 @@ public class ImportManager {
 	 * 
 	 * @param propsFile
 	 */
-	/*
-	 * public void importFromSalesForce(String projectName, String modelName, String connectionProfile, String
-	 * propsFile){ Properties props = new TeiidBot().getProperties(propsFile); this.importFromSalesForce(projectName,
-	 * modelName, connectionProfile, props); }
-	 */
-
 	public void importFromSalesForce(String projectName, String modelName, String connectionProfile, Properties props) {
 		SalesforceImportWizard sfWizard = new SalesforceImportWizard();
 		sfWizard.setConnectionProfile(connectionProfile);
@@ -36,65 +26,4 @@ public class ImportManager {
 
 		sfWizard.execute();
 	}
-
-	/**
-	 * Import metadata from Oracle, HSQLDB, SQL Server
-	 * 
-	 * @param connectionProfile
-	 * @param propsFile
-	 */
-	/*
-	 * public void importFromDatabase(String projectName, String modelName, String connectionProfile, String propsFile){
-	 * Properties props = new TeiidBot().getProperties(propsFile); importFromDatabase(projectName, modelName,
-	 * connectionProfile, props); }
-	 */
-	// TODO: refactor the parameters
-	public void importFromLdap(String projectName, String modelName, String connectionProfile, String connectionUrl,
-			String principalDnSuffix, Properties props) {
-
-		LdapImportWizard wizard = new LdapImportWizard();
-		wizard.setConnectionProfile(connectionProfile);
-		wizard.setModelName(modelName);
-		wizard.setProjectName(projectName);
-		wizard.setConnectionUrl(connectionUrl);
-		wizard.setPrincipalDnSuffix(principalDnSuffix);
-		String objects;
-		if ((objects = props.getProperty("selectedEntries")) != null) {
-			wizard.setSelectedEntries(objects.split(","));
-		}
-		if ((objects = props.getProperty("selectedColumns")) != null) {
-			wizard.setSelectedColumns(objects.split(","));
-		}
-		wizard.execute();
-	}
-
-	/*
-	 * public void importFromFlatFile(String projectName, String modelName, String connectionProfile, String propsFile){
-	 * Properties props = new TeiidBot().getProperties(propsFile); importFromFlatFile(projectName, modelName,
-	 * connectionProfile, props); }
-	 */
-
-	public static void importModel(String projectName, TeiidImportWizard importWizard) {// import mgr
-		ModelProject modelProject = new TeiidBot().modelExplorer().getModelProject(projectName);
-		modelProject.importModel(importWizard);
-	}
-
-	/*
-	 * public void importFromDDL(String projectName, String modelName, String cpName, String propsFile){ Properties
-	 * props = teiidBot.getProperties(propsFile); importFromDDL(projectName, modelName, cpName, props); }
-	 */
-
-	/*
-	 * public void importFromXML(String projectName, String modelName, String cpName, String propsFile){ Properties
-	 * props = teiidBot.getProperties(propsFile); importFromXML(projectName, modelName, cpName, props); }
-	 */
-
-	/**
-	 * WSDL File or URL >> Source and View Model (SOAP)
-	 */
-	/*
-	 * public void importFromWSDLToSrcView(String projectName, String modelName, String cpName, String propsFile){
-	 * Properties props = teiidBot.getProperties(propsFile); importFromWSDLToSrcView(projectName, modelName, cpName,
-	 * props); }
-	 */
 }
